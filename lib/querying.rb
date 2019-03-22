f7905b7ddf8a4e2d1094c4dd4f438ad536ad79b6
John Hayes <johnnyhayes7@gmail.com>
2019-03-22 17:50:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,9 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT name, subgenres.title FROM authors
    JOIN series
-   ON authors.id = series.author_id"
+   ON authors.id = series.author_id
+   JOIN subgenres
+   ON series.id = subgenres.series_id"
 end
 
 def select_series_title_with_most_human_characters
