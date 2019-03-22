0ad0c1e17af54ad159ff3c28420adc5892b34d7a
John Hayes <johnnyhayes7@gmail.com>
2019-03-22 18:22:29 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -23,7 +23,7 @@ def select_series_title_with_most_human_characters
   "SELECT title FROM series
    JOIN characters
    ON characters.series_id = series.id
-   GROUP BYcharacters.species"
+   GROUP BY characters.species"
 end
 
 def select_character_names_and_number_of_books_they_are_in
