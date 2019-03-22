bb5a7b6ca92f8e807e397fd64d7a2b9e79a3f2df
John Hayes <johnnyhayes7@gmail.com>
2019-03-22 16:47:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -23,5 +23,5 @@ INSERT INTO subgenres (name) VALUES ("Sports");
 INSERT INTO authors (name) VALUES ("John Hayes");
 INSERT INTO authors (name) VALUES ("Jennifer Singer");
 
-INSERT INTO character_books (character_id, book_id) VALUES (1, 1)
+INSERT INTO character_books (character_id, book_id) VALUES (1, 1);
 INSERT INTO character_books (character_id, book_id) VALUES (2, 1)
