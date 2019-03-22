d0412765bc81ba333ddd90252da60906e13652c3
John Hayes <johnnyhayes7@gmail.com>
2019-03-22 16:38:59 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -15,3 +15,4 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Pep
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Sammy", "Eata?", "Human", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Tristan", "Awww Yeaaahh", "Human", 2, 2);
 INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Christian", "I will Destroy", "Human", 2, 2);
+INSERT INTO characters (name, motto, species, author_id, series_id) VALUES ("Owen", "Mario!", "Human", 2, 2);
