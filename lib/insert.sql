a0b952f9329526c7cf008c986f7956e578ff9f76
John Hayes <johnnyhayes7@gmail.com>
2019-03-22 16:24:25 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,4 +3,4 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES("Groundball to Short",
 
 INSERT INTO books (title, year, series_id) VALUES ("A Deadly Walk", 2010, 1);
 INSERT INTO books (title, year, series_id) VALUES ("A Deadlier Walk", 2012, 1);
-INSERT INTO series (title, year, series_id) VALUES ("A Deadlier Walk", 2012, 1);
+INSERT INTO books (title, year, series_id) VALUES ("A Deadlier Walk", 2012, 1);
