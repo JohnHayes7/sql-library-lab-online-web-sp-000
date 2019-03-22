034823b85b5f1b2047cfcc5516dd42ffbe3510e7
John Hayes <johnnyhayes7@gmail.com>
2019-03-22 16:27:20 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -5,3 +5,4 @@ INSERT INTO books (title, year, series_id) VALUES ("A Deadly Walk", 2010, 1);
 INSERT INTO books (title, year, series_id) VALUES ("A Deadlier Walk", 2012, 1);
 INSERT INTO books (title, year, series_id) VALUES ("We Should Turn Around", 2014, 1);
 INSERT INTO books (title, year, series_id) VALUES ("The Short Hop", 2011, 2);
+INSERT INTO books (title, year, series_id) VALUES ("The Bobble", 2012, 2);
