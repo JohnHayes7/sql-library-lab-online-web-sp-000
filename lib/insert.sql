74131536dfcf205a090620a545eb2fcd551697bf
John Hayes <johnnyhayes7@gmail.com>
2019-03-22 16:47:22 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -24,3 +24,4 @@ INSERT INTO authors (name) VALUES ("John Hayes");
 INSERT INTO authors (name) VALUES ("Jennifer Singer");
 
 INSERT INTO character_books (character_id, book_id) VALUES (1, 1)
+INSERT INTO character_books (character_id, book_id) VALUES (2, 1)
